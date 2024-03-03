24032c3dacede5a3d7bab007a7fe9ff10e691106
pyroshary <162053201+pyroshary@users.noreply.github.com>
2024-03-03 14:19:12 -0700

    Add files via upload

---
+++ cases.ts
@@ -0,0 +1,4 @@
+let person_name: string= "shary";
+console.log(`lowercase:${person_name. toLowerCase()}`);
+console.log(`uppercase:${person_name. toUpperCase()}`);
+console.log(person_name.charAt(0).toUpperCase() + person_name.slice(1).toLowerCase()); 
