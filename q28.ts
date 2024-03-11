87dc956c538dff94ba377186ff8e87e4093f6dcc
pyroshary <162053201+pyroshary@users.noreply.github.com>
2024-03-12 04:00:41 +0500

    Add files via upload

---
+++ q28.ts
@@ -0,0 +1,6 @@
+ let age: number = 20; 
+ if (age < 2){ console.log("This is a baby");}
+else if(age< 4){console.log("This is 4 years");}
+else if(age< 6){console.log("This is 6 years");}
+else if(age< 8){console.log("This is 8 years");}
+else {console.log("This is big man");}
---
+++ q30.ts
@@ -0,0 +1,6 @@
+ let officers : string[] = ["ali" , "rehman","kamran","imran","khan"];
+ officers.forEach(officer => {if(officer==="khan")
+  {console.log(`hello ${officer} sir how are you?`)}else{console.log(`hello ${officer} hello how may i help u?`)}});
+
+
+ 
