461a15e1759bcd04d764d0c83e143bf888246b12
pyroshary <162053201+pyroshary@users.noreply.github.com>
2024-03-16 02:40:30 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,18 +1,4 @@
-let cites: string[]=  [ "japan","iran","turkey","china"];
-console.log("original order:" ,cites);
-//to print array in alphabetical order with out modify
-console.log("alphabatical order:",[...cites].sort());
-// to print array in original order
-console.log(" original order:",cites);
-// to print array in reverse order without modify
-console.log("reverse order :",[...cites].reverse());
-//to print the array in original order
-console.log(" original order:",cites);
-// to print the array in origianl and change the original
-console.log("original reverse:",cites.reverse());
-// to print the array in reverse to show that its in orginal condition
-console.log("back to original order:",cites.reverse());
-//to print the array in original in alphabetical
-console.log("original into alphabetical order:",cites.sort());
-//to print array in reverse to origial again
-console.log("original reverse:",cites.reverse());
+ function trip(city :string , country :string): String{return `${city}, ${country}`};
+ console.log(trip("lahore","pakistan"));
+ console.log(trip("china","japan"));
+ console.log(trip("delhi","india"));
